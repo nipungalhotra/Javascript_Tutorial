c73d12795913a3814d9281d9106d425177769358
nipungalhotra <82778190+nipungalhotra@users.noreply.github.com>
2024-07-10 05:49:08 +0000

    conversion of data types

---
+++ 01_Basics/Conversion_Operator.js
@@ -0,0 +1,26 @@
+let score = "Instagram"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber)
+
+
+//"33" is convert into number
+//"33abc" is not convert into number output =>Nan which is Number 
+//true/false  convert into 1/0 
+
+let isLoggedIn = 1 
+let BooleanLoggedIn = Boolean(isLoggedIn)
+console.log(BooleanLoggedIn)
+
+//1 => true; 0 => false
+// "" => false
+// "instagram" => true
+
+let someNumber = 7
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof(stringNumber))
