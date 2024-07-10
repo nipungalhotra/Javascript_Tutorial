af894b02d67a8bff815d518885e72d475894a00c
nipungalhotra <82778190+nipungalhotra@users.noreply.github.com>
2024-07-10 08:34:34 +0000

    Compairson

---
+++ 01_Basics/Comparison.js
@@ -0,0 +1,22 @@
+//console.log(2>1)
+//console.log(2>=1)
+//console.log(2<1)
+//console.log(3==1)
+//console.log(2!=1)
+
+
+//console.log("2">1);
+//console.log("02">1);
+//console.log(null>0);
+//console.log(null<0);
+//console.log(null==0);
+//console.log(null<=0);
+//console.log(null>=0);
+/*The Reson of that equality check == and comparision > <> <= >= work differently  */
+/*Comparisons Convert null to a number, treating it as 0 That's Why null >= 0 is true and null > 0 is False  */
+//console.log(undefined==0);
+//console.log(undefined >= 0);
+//console.log(undefined > 0);
+
+// === Strict Check by checking datatype 
+console.log("2"===2)
