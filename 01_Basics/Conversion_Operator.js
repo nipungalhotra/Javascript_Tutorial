337b369911424e9cea19af94d544bf7422b22b1d
nipungalhotra <82778190+nipungalhotra@users.noreply.github.com>
2024-07-10 08:11:37 +0000

    Conversion Operator

--- 01_Basics/Conversion_Operator.js
+++ 01_Basics/Conversion_Operator.js
@@ -14,7 +14,7 @@ let valueInNumber = Number(score)
 
 let isLoggedIn = 1 
 let BooleanLoggedIn = Boolean(isLoggedIn)
-console.log(BooleanLoggedIn)
+//console.log(BooleanLoggedIn)
 
 //1 => true; 0 => false
 // "" => false
@@ -22,5 +22,36 @@ console.log(BooleanLoggedIn)
 
 let someNumber = 7
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof(stringNumber))
+//console.log(stringNumber)
+//console.log(typeof(stringNumber))
+/*****************************Operator******************/
+let value = 3 
+let negValue = -value
+console.log(negValue)
+//console.log(2+2)
+//console.log(2-2)
+//console.log(2*2)
+//console.log(2**3)
+//console.log(2/3)
+//console.log(2%3)
+
+
+let str1 ="hello"
+let str2 ="Nipun"
+let str3 = str1+str2
+//console.log(str3)
+
+
+//console.log(-true)// not preferable in MNC
+//console.log(+"")// not used 
+
+let num1, num2, num3 
+ 
+
+num1=num2=num3 =12+2 // also not preferable 
+
+let gameCounter =100
+//gameCounter++; // prefix operator If used postfix, with operator after operand (for example, x++), the increment operator increments and returns the value before incrementing.
+//++gameCounter; //postfic operator If used prefix, with operator before operand (for example, ++x), the increment operator increments and returns the value after incrementing.
+console.log(gameCounter)
+
