ed03f4d57face1382e87ad810d6c65941f8652f4
nipungalhotra <82778190+nipungalhotra@users.noreply.github.com>
2024-07-10 14:31:26 +0000

    Numb_and_Math

---
+++ 01_Basics/Number_Math.js
@@ -0,0 +1,31 @@
+const score = 400
+//console.log(score)
+const balance = new Number(100)
+//console.log(balance);
+
+//console.log(balance.toString().length)
+//console.log(balance.toFixed(5))
+
+
+const otherNumber =1123.985
+//console.log(otherNumber.toPrecision(4))
+
+const hundered = 100000
+//console.log(hundered.toLocaleString('en-IN'))
+//console.log(hundered.toLocaleString())
+
+//**************************Maths*************/
+//console.log(Math.abs(-4))
+//console.log(Math.round(4.632))
+//console.log(Math.ceil(4.632))
+//console.log(Math.floor(4.632))
+//console.log(Math.min(4,6,3,2))
+//console.log(Math.max(4,6,3,2))
+//console.log(Math.random(4,6,3,2))
+
+console.log(Math.random()*10)
+console.log((Math.random()*10)+1)
+
+const min = 10
+const max = 20 
+console.log(Math.floor(Math.random()* (max-min + 1))+min )
