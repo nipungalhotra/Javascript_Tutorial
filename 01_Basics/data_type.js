b63b82349e53a61ac4ee16a3dd1605e4195b4b37
nipungalhotra <82778190+nipungalhotra@users.noreply.github.com>
2024-07-10 10:55:16 +0530

    hands on Primitive Data types

---
+++ 01_Basics/data_type.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all JS code as newer version 
+
+// alert(3+3) // we are using Node.js,not browser 
+console.log(3+3); 
+console.log("Nipun")
+let name = "Nipun"
+let age = 16
+let isLoggedIn = false
+
+
+//Primitive Data type 
+//number =>2 to power 53
+// bigInt
+//string => ""
+//boolean => True/ False 
+//null => standalone value (representation of  empty string)
+//undefined => value is not defined 
+//symbol => unique 
+
+//object 
+console.log(typeof null)
